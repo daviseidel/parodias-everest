d325f9242a4b91e1b19167a611b58729c057d2a0
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-09-08 04:02:08 -0300

    refactor: limpei o codigo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,37 +2,20 @@
   import PocketBase from 'pocketbase';
   import eventsource from 'eventsource';
   global.EventSource = eventsource;
-  import {
-    afterNavigate,
-    beforeNavigate,
-    disableScrollHandling,
-    goto,
-    invalidate,
-    invalidateAll,
-    onNavigate,
-    preloadCode,
-    preloadData,
-    pushState,
-    replaceState
-  } from '$app/navigation';
 
   const url = 'https://parodias-everest.pockethost.io/'
   const pb = new PocketBase(url)
-  let isCaregado;
-  let evento = {}
 
   export let data;
   let parodia = data.records.items[0].expand.principal;
-  let principal = parodia.id
 
   pb.collection('evento').subscribe('*',async function (e) {
     console.log(e.action);
     // console.log(e.record); 
     parodia = await pb.collection('parodias').getOne(e.record.principal)
-    console.log(parodia)
-  });
-  
+  });  
 </script>
+
 {#key parodia}
   <h1 class="h1 text-center my-1">{parodia.musica}</h1>
   <h4 class="h4 text-center my-5">Grupo: {parodia.nomes}</h4>
